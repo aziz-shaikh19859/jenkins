a2ab7d9025aa6e357375949e135dc872e67590e5
aziz-shaikh19859 <150433064+aziz-shaikh19859@users.noreply.github.com>
2024-01-14 11:35:48 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "s3-backend-19859"
+    bucket = "my19859"
     key    = "terraform.tfstate"
-    region = "ap-south-1"
+    region = "us-east-1"
   }
 
   
